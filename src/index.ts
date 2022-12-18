7685548e6efccae868f0058d0fd0b61ef861398c
thais <trevillon@jehann.fr>
2022-12-18 23:05:02 +0100

    :memo: configurations for publishing

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-import {Abra} from "./Abra";
+import Abra from "./Abra";
 import {Interceptor, HttpMethod, AbraConfigs} from "./types";
+
+export default Abra;
+export {Interceptor, HttpMethod, AbraConfigs};
