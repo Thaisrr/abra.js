0ac00cf4b3b5ef91739b3c3147e87b27a26267f9
thais <trevillon@jehann.fr>
2022-12-19 23:24:03 +0100

    :test_tube: :white_check_mark: Writing tests ( all passes except error )

---
+++ register.js
@@ -0,0 +1,12 @@
+/**
+ * Overrides the tsconfig used for the app.
+ */
+
+const tsNode = require('ts-node');
+const testTSConfig = require('./tests/tsconfig.json');
+
+tsNode.register({
+    files: true,
+    transpileOnly: true,
+    project: './tests/tsconfig.json'
+});
